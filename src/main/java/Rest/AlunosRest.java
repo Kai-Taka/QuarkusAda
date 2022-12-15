68fbf421ef647e4f1b22ca3c7ccb3165792dcd28
F6071778 Kawe Takamoto <kawe.takamoto@bb.com.br>
2022-12-15 18:21:05 +0000

    added update endpoints

--- src/main/java/Rest/AlunosRest.java
+++ src/main/java/Rest/AlunosRest.java
@@ -8,6 +8,7 @@ import javax.print.attribute.standard.Media;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
@@ -79,6 +80,26 @@ public class AlunosRest {
                 .status(Response.Status.NOT_FOUND)
                 .entity("Não há aluno com este id")
                 .type(MediaType.TEXT_PLAIN)
+                .build();
+    }
+
+    @Path("/{id}")
+    @PUT
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response updateAluno(String name, @PathParam("id") Integer id)
+    {
+        if (db.get(id) == null)
+        {
+            return Response
+                    .status(Response.Status.NOT_FOUND)
+                    .entity("Não há aluno com esse id")
+                    .build();
+        }
+        db.get(id).setName(name);
+
+        return Response
+                .status(Response.Status.OK)
+                .entity(db.get(id))
                 .build();
     }
 }
